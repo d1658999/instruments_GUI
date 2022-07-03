39d9f353911bd7cc345b3d7ccf1f469f5c6817fc
PriceWu <pricewu@google.com>
2022-07-03 23:17:01 +0800

    fix LTE b41 the GUI bugs that is return the type from string to int

--- ui_init.py
+++ ui_init.py
@@ -1,12 +1,12 @@
-tech = ['FR1']
-bw_fr1 = [10]
-bw_lte = [20]
+tech = ['LTE', 'FR1']
+bw_fr1 = [5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100]
+bw_lte = [1.4, 3, 5, 10, 15, 20]
 ue_power = [1, 0]
 
 chan = "LMH"
 
-bands_fr1 = [3]
-bands_lte = [3]
+bands_fr1 = [1, 2, 3, 7, 25, 66, 30, 40, 38, 41]
+bands_lte = [1, 2, 3, 4, 7, 25, 66, 30, 39, 40, 38]
 bands_wcdma = []
 bands_hsupa = []
 bands_hsdpa = []
@@ -14,7 +14,7 @@ bands_gsm = None
 bands_endc = ['3_78']
 
 tx = True
-rx = False
+rx = True
 rx_sweep = False
 tx_level_sweep = False
 tx_freq_sweep = False
@@ -29,7 +29,7 @@ port_tx_fr1 = 4
 sa_nas = 0
 asw_path = 0
 srs_path = 0
-srs_path_enable = True
+srs_path_enable = False
 sync_path = "Main"
 
 
@@ -37,8 +37,8 @@ tx_paths = ['TX1']
 rx_paths = [2]
 
 scripts = ['GENERAL']
-type_fr1 = ['DFTS']
+type_fr1 = ['DFTS', 'CP']
 mcs_lte = ['QPSK', 'Q16', 'Q64', 'Q256']
-mcs_fr1 = ['QPSK']
+mcs_fr1 = ['BPSK', 'QPSK', 'Q16', 'Q64', 'Q256']
 rb_ftm_lte = ['PRB', 'FRB']
-rb_ftm_fr1 = ['INNER_FULL']
+rb_ftm_fr1 = ['INNER_FULL', 'OUTER_FULL', 'INNER_1RB_LEFT', 'INNER_1RB_RIGHT', 'EDGE_1RB_LEFT', 'EDGE_1RB_RIGHT', 'EDGE_FULL_LEFT', 'EDGE_FULL_RIGHT']
