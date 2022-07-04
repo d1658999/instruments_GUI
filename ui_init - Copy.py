b2a7cd309dd4e3ffc5383d12c985d8eb2b6c844f
PriceWu <pricewu@google.com>
2022-07-04 17:00:24 +0800

    update some backup

---
+++ ui_init - Copy.py
@@ -0,0 +1,17 @@
+tech = []
+bw = []
+ue_power = []
+
+test_item = {'tx': True, 'rx': False, 'rx_sweep': False}
+
+band_lte = [1, 2]
+band_wcdma = []
+band_hsupa = []
+band_hsdpa = []
+band_gsm =[]
+
+tx = []
+rx = []
+rx_sweep = []
+
+instrument = ''
